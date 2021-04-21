6db6cd34a1572c1439ada76141cbc40e85b49b39
Adrian Hartanto <adrianfile1@gmail.com>
2021-04-21 23:26:57 +0700

    Change Margin

--- src/components/promos.js
+++ src/components/promos.js
@@ -6,7 +6,7 @@ const Promos = () => (
             <p className="text-3xl rajdhani text-white text-center">Beruntung kamu berada disini !</p>
             <br></br>
             <p className="text-justify px-3 italic">Dengan adanya Mooosik untukmu, kami senantiasa memberikanmu sebuah hadiah yang membekas untukmu. Yaitu Trial Account selama 2 bulan untuk kamu dengarkan. Dan kami tidak lupa untuk memberikanmu janji Top Up pertamamu di Aplikasi kami.</p>
-            <a href="#top" className="bg-white text-right px-5 rounded-full rajdhani italic float-right mt-8 hover:bg-black hover:text-white transition ease duration-300">Selengkapnya</a>
+            <a href="#top" className="bg-white text-right px-5 rounded-full rajdhani italic float-right mt-2 hover:bg-black hover:text-white transition ease duration-300">Selengkapnya</a>
         </div>
     </section>
 )
